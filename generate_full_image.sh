f15e7aa397a0ce6d153c183c0170d2117777ced7
Lia <liazerqueraf@gmail.com>
2023-09-12 15:48:15 -0400

    fix bug in .sh

--- generate_full_image.sh
+++ generate_full_image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-contribs=(sklearn nltk gensim regex keras spacy streamlit telegram transformers wikipedia)
-docker build . -t autogoal/autogoal:all-contribs -f dockerfiles/development/dockerfile --build-arg extras="common $contribs remote" --no-cache
+contribs=(sklearn nltk gensim regex keras )
+docker build . -t autogoal/autogoal:full_image -f dockerfiles/development/dockerfile --build-arg extras="common $contribs remote" --no-cache

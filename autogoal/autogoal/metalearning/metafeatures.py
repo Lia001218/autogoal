0102c3178eadbe36fad04c7c1a9d38a395c664be
Lia <liazerqueraf@gmail.com>
2023-10-09 00:48:26 -0400

    done text metafeatures

--- autogoal/autogoal/metalearning/metafeatures.py
+++ autogoal/autogoal/metalearning/metafeatures.py
@@ -1,6 +1,9 @@
 from scipy.stats import skew, kurtosis
 import numpy as np
 from scipy import stats
+from distfit import distfit
+from lexical_diversity import lex_div as ld
+from nltk.corpus import stopwords
 
 class MetafeatureExtractor:
     def __init__(self):
@@ -15,13 +18,10 @@ class TabularMetafeatureExtractor(MetafeatureExtractor):
         self.features = []
 
     def extract_features(self, X, y=None):
-        if y == None: # is supervised
-            self.features.append(1)
-        else : 
-            self.features.append(0)
-        self.features.append(X.shape[0]) #cardinality
-        self.features.append(X.shape[1]) #attributes
-        self.features.append(X.shape[0]/X.shape[1]) # proportion cardinality/attributes
+        self.__issupervised__(y)
+        self.features.append(len(X.shape[0])) #cardinality
+        self.features.append(len(X.shape[1])) #attributes
+        self.features.append(len(X.shape[0])/len(X.shape[1])) # proportion cardinality/attributes
         numeric_values = self.__numeric_values__(X)
         self.features.append(numeric_values) #number of numeric attrubutes
         missing_values = self.__missing_values__(X)
@@ -47,6 +47,12 @@ class TabularMetafeatureExtractor(MetafeatureExtractor):
         cummon_info = self.__mutual_information__(X,y,standardization_factor)
         self.__equivalent_number_of_attr__(standardization_factor,cummon_info)
         self.__noise_signal_ratio__(cummon_info,attr_entropy)
+
+    def __issupervised__(self,y):
+        if y == None: # is supervised
+            self.features.append(1)
+        else : 
+            self.features.append(0)
 
     def __normalized_class_entropy__(self,X, y):
         if y :
@@ -136,8 +142,22 @@ class ImageMetafeatureExtractor(MetafeatureExtractor):
 
 
     def extract_features(self, X, y=None):
-        pass
+        self.__issupervised__(y)
+        self.features.append(len(X.shape[0]))
+        self.__output_cardinality__(y)
 
+
+    def __output_cardinality__(self,y):
+        if y == None: 
+            self.features.append(-1)
+        else : 
+            self.features.append(len(y))
+    
+    def __issupervised__(self,y):
+        if y == None: # is supervised
+            self.features.append(1)
+        else : 
+            self.features.append(0)
 
 class TextMetafeatureExtractor(MetafeatureExtractor):
     def __init__(self, X, y=None):
@@ -145,4 +165,66 @@ class TextMetafeatureExtractor(MetafeatureExtractor):
 
 
     def extract_features(self, X, y=None):
-        pass
+        self.__issupervised__(y)
+        cardinality = self.features.append(len(X.shape[0]))
+        output_cardinality = self.__output_cardinality__(y)
+        self.features.append(cardinality/output_cardinality)
+        self.__average_len__(X)
+        self.__len_distribution__(X)
+        self.__lexical_diversity__(X)
+        self.__stop_words_proportion__(X)
+
+    def __average_len_words__(self,X):
+        average = []
+        for x in X.shape[0]:
+            large = 0
+            for words in x:
+                large += len(words)
+            average.append(large/len(x))
+        average = np.array(average)
+        self.features.append(average.mean())
+        self.features.append(average.min())
+        self.features.append(average.max())
+        self.features.append(average.std())
+
+    def __stop_words_proportion__(self,X):
+        stopwords_proportion = []
+        for x in X.shape[0]:
+            count =0
+            for words in ld.tokenize(x):
+                if words in stopwords:
+                    count +=1
+            stopwords_proportion.append(count/len(x))
+        stopwords_proportion = np.array(stopwords_proportion)
+        self.features.append(stopwords_proportion.mean())
+        self.features.append(stopwords_proportion.min())
+        self.features.append(stopwords_proportion.max())
+        self.features.append(stopwords_proportion.std())
+
+    def __lexical_diversity__(self,X):
+        list_lexical_diversity = np.array([ld.ttr(ld.tokenize(x)) for x in X.shape[0]])
+        self.features.append(list_lexical_diversity.mean())
+        self.features.append(list_lexical_diversity.min())
+        self.features.append(list_lexical_diversity.max())
+        self.features.append(list_lexical_diversity.std())
+
+    def __len_distribution__(self, X):
+        doc_len = np.array([len(x) for x in X.shape[0]])
+        dift = distfit()
+        sumary = dift.fit_transform(doc_len)
+        self.features.append(sumary['model']['name'])
+
+    def __average_len__(self,X):
+        len_doc = np.array([len(x) for x in X.shape[0] ])
+        self.features.append(len_doc.mean())
+    def __output_cardinality__(self,y):
+        if y == None: 
+            self.features.append(-1)
+        else : 
+            self.features.append(len(y))
+    
+    def __issupervised__(self,y):
+        if y == None: # is supervised
+            self.features.append(1)
+        else : 
+            self.features.append(0)

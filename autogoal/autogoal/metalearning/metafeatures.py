ba10b366e1ceb338970665864d3ccadc7aabfbe4
Lia <liazerqueraf@gmail.com>
2023-10-12 15:57:45 -0400

    done metafeatures tabular

--- autogoal/autogoal/metalearning/metafeatures.py
+++ autogoal/autogoal/metalearning/metafeatures.py
@@ -1,6 +1,7 @@
 from scipy.stats import skew, kurtosis
 import numpy as np
 from scipy import stats
+from scipy.sparse import csr_matrix
 from distfit import distfit
 from lexical_diversity import lex_div as ld
 from nltk.corpus import stopwords
@@ -19,43 +20,53 @@ class TabularMetafeatureExtractor(MetafeatureExtractor):
 
     def extract_features(self, X, y=None):
         self.__issupervised__(y)
-        self.features.append(len(X.shape[0])) #cardinality
-        self.features.append(len(X.shape[1])) #attributes
-        self.features.append(len(X.shape[0])/len(X.shape[1])) # proportion cardinality/attributes
-        numeric_values = self.__numeric_values__(X)
+        
+        # print('x: ', X)
+        if isinstance(X, csr_matrix):
+            X = X.toarray()
+
+        self.features.append(X.shape[0]) #cardinality
+        self.features.append(X.shape[1]) #attributes
+        self.features.append(X.shape[0]/X.shape[1]) # proportion cardinality/attributes
+        missing_values = np.argwhere(np.isnan(X))
+        numeric_values = len(X.shape) - len(missing_values)
         self.features.append(numeric_values) #number of numeric attrubutes
-        missing_values = self.__missing_values__(X)
-        self.features.append(missing_values)
-        self.features.append(len(X.shape) - numeric_values - missing_values )
-        self.features.append(X.std())
+        self.features.append(len(missing_values))
+        self.features.append(np.std(X))
         self.features.append(X.std()/X.shape[1])
-        self.features.append(X.T.cov().mean())
-        self.features.append(X.T.cov().mean()/ X.std())
+     
         skewness = np.array([skew(row) for row in X.T])
         self.features.append(skewness.min())
         self.features.append(skewness.max())
         self.features.append(skewness.std())
         self.features.append(skewness.mean())
+
         kurtosis_val = np.array([kurtosis(row) for row in X.T])
+        
         self.features.append(kurtosis_val.min())
         self.features.append(kurtosis_val.max())
         self.features.append(kurtosis_val.std())
         self.features.append(kurtosis_val.mean())
+  
         standardization_factor = self.__normalized_class_entropy__(X,y)
         attr_entropy = self.__normalized_attr_entropy__(X)
+
         self.__joint_entropy__(X,y)
+
         cummon_info = self.__mutual_information__(X,y,standardization_factor)
         self.__equivalent_number_of_attr__(standardization_factor,cummon_info)
         self.__noise_signal_ratio__(cummon_info,attr_entropy)
+        print('finish')
+        return self.features
 
     def __issupervised__(self,y):
-        if y == None: # is supervised
+        if y is None: # is supervised
             self.features.append(1)
         else : 
             self.features.append(0)
 
     def __normalized_class_entropy__(self,X, y):
-        if y :
+        if not y is None:
             _, counts = np.unique(y, return_counts=True)
             self.features.append(stats.entropy(counts) / np.log2(len(counts)))
             return stats.entropy(counts) / np.log2(len(counts))
@@ -70,10 +81,11 @@ class TabularMetafeatureExtractor(MetafeatureExtractor):
     def __normalized_attr_entropy__(self,X):
         attributes = [X[:, j] for j in range(X.shape[1])]
         attr_entropy = np.array([self.__attr_i_entropy__(xi) for xi in attributes])
+        attr_entropy = np.nan_to_num(attr_entropy,nan= -1)
         self.features.append(attr_entropy.mean())
         self.features.append(attr_entropy.max())
         self.features.append(attr_entropy.std())
-        self.features.append(attr_entropy.mean())
+        self.features.append(attr_entropy.min())
         return attr_entropy.mean()
         
     def __equivalent_number_of_attr__(self,standardization_factor,cummon_info):
@@ -94,6 +106,7 @@ class TabularMetafeatureExtractor(MetafeatureExtractor):
                 joint_entropy = self.__attr_i_joint_entropy__(X[:, j], y)
                 result.append(standardization_factor + attr_entropy - joint_entropy)
         result = np.array(result)
+        result = np.nan_to_num(result, nan= -1)
         self.features.append(result.mean())
         self.features.append(result.max())
         self.features.append(result.std())
@@ -102,14 +115,15 @@ class TabularMetafeatureExtractor(MetafeatureExtractor):
     
 
     def __joint_entropy__(self,X, y):
-        if y == None:
+        if y is None:
             return -1
         attributes_labels = [(X[:, j], y) for j in range(X.shape[1])]
         joint_entropy = np.array([self.__attr_i_joint_entropy__(attr_i, y) for attr_i, y in attributes_labels])
         self.features.append(joint_entropy.mean())
         self.features.append(joint_entropy.max())
         self.features.append(joint_entropy.std())
-        self.features.append(joint_entropy.mean())
+        self.features.append(joint_entropy.min())
+
 
     def __attr_i_joint_entropy__(self,x_i, y):
         _, counts = np.unique(list(zip(x_i, y)), return_counts=True)
@@ -124,10 +138,12 @@ class TabularMetafeatureExtractor(MetafeatureExtractor):
         return count
     
     def __missing_values__(self, X):
-        total = np.isnan(X)
-        for _ in range(len(X.shape) - 1):
-            total = total.sum()
-        return int(total)
+        total = 0
+        for i in X:
+            if i is np.isnan:
+                total +=1
+        self.features.append(total)
+        return total
     
     def __noise_signal_ratio__(self,cummon_info,attr_entropy ):
         self.features.append(attr_entropy /cummon_info)
@@ -143,18 +159,48 @@ class ImageMetafeatureExtractor(MetafeatureExtractor):
 
     def extract_features(self, X, y=None):
         self.__issupervised__(y)
-        self.features.append(len(X.shape[0]))
+        self.features.append(X.shape[0])
         self.__output_cardinality__(y)
+        self.__average_size__(X)
+        self.__type_image__(X)
+
+
+    def __type_image__(self,X):
+        twoD = False
+        threeD = False
+        for x in X : 
+            if len(x) == 2:
+                twoD = True
+            elif len(x) == 3:
+                threeD = True
+            if twoD and threeD:
+                self.features.append(2)
+                break
+        if twoD:
+            self.features.append(0)
+        if threeD:
+            self.features.append(1)
+
+
+    def __average_size__(self,X):
+        total_height = 0
+        total_width = 0
+        for x in X:
+            height, width, _ = x.shape
+            total_height += height
+            total_width += width
+        self.features.append(total_height/len(X))
+        self.features.append(total_width/len(X))
 
 
     def __output_cardinality__(self,y):
-        if y == None: 
+        if y is None: 
             self.features.append(-1)
         else : 
             self.features.append(len(y))
     
     def __issupervised__(self,y):
-        if y == None: # is supervised
+        if y is None: # is supervised
             self.features.append(1)
         else : 
             self.features.append(0)
@@ -166,7 +212,7 @@ class TextMetafeatureExtractor(MetafeatureExtractor):
 
     def extract_features(self, X, y=None):
         self.__issupervised__(y)
-        cardinality = self.features.append(len(X.shape[0]))
+        cardinality = self.features.append(X.shape[0])
         output_cardinality = self.__output_cardinality__(y)
         self.features.append(cardinality/output_cardinality)
         self.__average_len__(X)
@@ -218,13 +264,13 @@ class TextMetafeatureExtractor(MetafeatureExtractor):
         len_doc = np.array([len(x) for x in X.shape[0] ])
         self.features.append(len_doc.mean())
     def __output_cardinality__(self,y):
-        if y == None: 
+        if y is None: 
             self.features.append(-1)
         else : 
             self.features.append(len(y))
     
     def __issupervised__(self,y):
-        if y == None: # is supervised
+        if y is None: # is supervised
             self.features.append(1)
         else : 
             self.features.append(0)

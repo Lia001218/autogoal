7c9f520c3454947913b578be55dabd988cfd868e
Lia <liazerqueraf@gmail.com>
2023-10-13 19:46:26 -0400

    create example with text dataset

---
+++ autogoal/tests/examples/automl_movie_review.py
@@ -0,0 +1,23 @@
+from autogoal.ml import AutoML
+from autogoal.datasets import movie_reviews
+from autogoal.search import (
+    PESearch,
+    RichLogger,
+)
+from autogoal.kb import Seq, Sentence, VectorCategorical, Supervised
+from autogoal_contrib import find_classes
+from sklearn.metrics import f1_score
+from autogoal.metalearning.metafeatures import TabularMetafeatureExtractor,TextMetafeatureExtractor,ImageMetafeatureExtractor
+from sklearn.model_selection import train_test_split
+
+test = AutoML(
+    name= 'movie_review',
+    dataset_type= TextMetafeatureExtractor(),
+    input=(Seq[Sentence], Supervised[VectorCategorical]),
+    output=VectorCategorical
+)
+
+X, y = movie_reviews.load()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.25)
+loggers = [RichLogger()]
+test.fit(X_train, y_train, logger=loggers)

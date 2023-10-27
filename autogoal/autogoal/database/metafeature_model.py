205d1cb0fc384df259c1478d070d0e67a3dbce08
Lia <liazerqueraf@gmail.com>
2023-10-27 00:31:06 -0400

    add attribute to metafeature model

--- autogoal/autogoal/database/metafeature_model.py
+++ autogoal/autogoal/database/metafeature_model.py
@@ -3,10 +3,11 @@ from odmantic import Field, Model
 from .pipeline_model import PipelineModel
 
 class MetafeatureModel(Model):
-    dataset_name : str
+
     metacaracteristic_model: Optional[list] = None
     metric: str
     input_type: str
     output_type: str
     pipelines : Optional[list[PipelineModel]] = None 
+    dataset_type: str
 

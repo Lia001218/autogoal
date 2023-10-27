a10b6caa54578522433762133df8c6c6c443811e
Lia <liazerqueraf@gmail.com>
2023-10-27 00:32:30 -0400

    prepare database for meta-models

---
+++ autogoal/autogoal/metalearning/prepare_data.py
@@ -0,0 +1,36 @@
+import numpy as np
+from odmantic import SyncEngine
+from autogoal.database.metafeature_model import MetafeatureModel
+
+db = SyncEngine(database= 'Metalearning')
+
+def create_db_from_tabular_metafeatures():
+    tabular_features_db = []
+    tabular_modality = db.find(MetafeatureModel, MetafeatureModel.dataset_type == 'TabularMetafeatureExtractor')
+    for i in tabular_modality:
+        if i.pipelines:
+            for j in i.pipelines:
+                current= [i.metacaracteristic_model,i.metric,j.algorithm_flow,j.eval_result]
+                tabular_features_db.append(current)
+    return np.array(tabular_features_db)
+
+def create_db_from_text_metafeatures():
+    text_features_db = np.array([])
+    text_modality = db.find(MetafeatureModel, MetafeatureModel.dataset_type == 'TextMetafeatureExtractor')
+    for i in text_modality:
+        if i.pipelines:
+            for j in i.pipelines:
+                current= [i.metacaracteristic_model,i.metric,j.algorithm_flow,j.eval_result]
+                text_features_db = np.append(text_features_db,current)
+    return np.array(text_features_db)
+
+def create_db_from_image_metafeatures():
+    image_features_db = np.array([])
+    image_modality = db.find(MetafeatureModel, MetafeatureModel.dataset_type == 'ImageMetafeatureExtractor')
+    for i in image_modality:
+        if i.pipelines:
+            for j in i.pipelines:
+                current= [i.metacaracteristic_model,i.metric,j.algorithm_flow,j.eval_result]
+                image_features_db = np.append(image_features_db,current)
+    return np.array(image_features_db)
+        

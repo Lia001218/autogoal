37dde241664d640ee6bd31120e4fa66b99cf945b
Lia <liazerqueraf@gmail.com>
2023-10-28 02:38:58 -0400

    config auto generation of tensorflow_addons wheel

--- dockerfiles/install-package.sh
+++ dockerfiles/install-package.sh
@@ -20,6 +20,14 @@ do
             cd /home/coder/autogoal/autogoal-contrib/autogoal_contrib && poetry install
             cd /home/coder/autogoal/autogoal-contrib && sudo pip install -e autogoal_contrib
         ;;
+        keras)
+            cd /home/coder/autogoal/autogoal-contrib/autogoal_keras && poetry install
+            cd /home/coder/autogoal/autogoal-contrib && sudo pip install -e autogoal_keras
+            cd /home/coder && wget https://github.com/bazelbuild/bazelisk/releases/download/v1.18.0/bazelisk-linux-arm64 && chmod +x bazelisk-linux-arm64  && sudo mv bazelisk-linux-arm64 /usr/local/bin/bazel 
+            bazel
+            cd /home/coder && git clone https://github.com/tensorflow/addons.git
+            cd /home/coder/addons && sudo python ./configure.py && sudo bazel build build_pip_pkg && sudo bazel-bin/build_pip_pkg artifacts && pip install artifacts/tensorflow_addons-*.whl
+        ;;
         *)
             cd "/home/coder/autogoal/autogoal-contrib/autogoal_$arg" && poetry install
             cd "/home/coder/autogoal/autogoal-contrib" && pip install -e "autogoal_$arg"

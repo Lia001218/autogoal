88136a640dd111d5ae1e66219ead6cdc52458d1d
Lia <liazerqueraf@gmail.com>
2023-11-10 14:00:56 -0500

    delete remote

--- scripts/generate_full_image.sh
+++ scripts/generate_full_image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 contribs=(sklearn nltk gensim regex keras )
-docker build . -t autogoal/autogoal:full_image -f dockerfiles/development/dockerfile --build-arg extras="common keras sklearn nltk gensim regex remote" --no-cache
+docker build . -t autogoal/autogoal:full_image -f dockerfiles/development/dockerfile --build-arg extras="common keras sklearn nltk gensim regex" --no-cache

6c08893df18373d352b71a02234dbc2e8024d2f0
Lia <liazerqueraf@gmail.com>
2023-12-18 00:51:36 -0500

    update autoGluon .py

--- autogoal/autogoal/metalearning/autoGluon.py
+++ autogoal/autogoal/metalearning/autoGluon.py
@@ -7,9 +7,13 @@ from sklearn.model_selection import train_test_split
 # pd_data.rename(columns={'415': 'target'}, inplace=True)
 # print(pd_data)
 # data_root_Y = np.load('X_train.npy')
-train_data = pd.read_csv('train_data.csv')
+train_data = pd.read_csv('text_train_data.csv')
+
 data = TabularDataset(train_data)
 # test_data = TabularDataset(data_root_Y)
 print(train_data)
 
-predictor = TabularPredictor(label='415').fit(train_data=data)
+predictor = TabularPredictor(label='394').fit(train_data=data, presets='medium_quality')
+
+results = predictor.evaluate('text_test_data.csv')
+print("results: \n", results)
